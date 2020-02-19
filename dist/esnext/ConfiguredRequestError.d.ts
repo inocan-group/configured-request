daeb928186b31474ecf905351916ee296522e3ee
Ken Snyder <ken@ken.net>
2020-02-19 04:07:43 -0800

    remove old transpiled assets

--- dist/esnext/ConfiguredRequestError.d.ts
+++
@@ -1,6 +0,0 @@
-import { HttpStatusCodes } from "common-types";
-export declare class RequestError extends Error {
-    code: string;
-    statusCode: number;
-    constructor(message: string, code?: string, statusCode?: HttpStatusCodes);
-}
